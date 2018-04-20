44f1edd0cec80eac6c3cfdc50d1cdf91f7f9de7a
Nick Boyd <nick.boyd@realpage.com>
2018-04-19 20:19:14 -0500

    Add .prettierrc and lint run script

--- src/components/movies/MovieButton.js
+++ src/components/movies/MovieButton.js
@@ -3,20 +3,23 @@ import PropTypes from "prop-types";
 import ReactDom from "react-dom";
 import MoviePanel from "./MoviePanel";
 
-
 class MovieButton extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
     }
 
     render() {
-        return <div>
-            <button className="btn movie-btn" 
-                onClick={this.props.compareMovieScores} 
-                data-score={this.props.score}>
-              {this.props.value}
-            </button>
-          </div>;
+        return (
+            <div>
+                <button
+                    className="btn movie-btn"
+                    onClick={this.props.compareMovieScores}
+                    data-score={this.props.score}
+                >
+                    {this.props.value}
+                </button>
+            </div>
+        );
     }
 }
 
